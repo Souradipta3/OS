37f74afd81103cd23231555e1135351a20fdd3aa
Souradipta Saha <137396269+Souradipta3@users.noreply.github.com>
2025-01-19 23:06:33 +0530

    Update a.c

--- 6/a.c
+++ 6/a.c
@@ -5,7 +5,7 @@
 int main() {
     int pipe_fd[2];
     pid_t pid;
-    char write_msg[] = "Hello from parent!";
+    char write_msg[50];
     char read_msg[50];
 
     if (pipe(pipe_fd) == -1) {
@@ -21,14 +21,20 @@ int main() {
     }
 
     if (pid == 0) {
-        close(pipe_fd[1]);
-        read(pipe_fd[0], read_msg, sizeof(read_msg));
+        
+        close(pipe_fd[1]); 
+        read(pipe_fd[0], read_msg, sizeof(read_msg)); 
         printf("Child received: %s\n", read_msg);
         close(pipe_fd[0]);
     } else {
+    
+        printf("Enter a message for the child: ");
+        fgets(write_msg, sizeof(write_msg), stdin); 
+        write_msg[strcspn(write_msg, "\n")] = '\0'; 
+
         close(pipe_fd[0]);
-        write(pipe_fd[1], write_msg, strlen(write_msg) + 1);
-        close(pipe_fd[1]);
+        write(pipe_fd[1], write_msg, strlen(write_msg) + 1); 
+        close(pipe_fd[1]); 
     }
 
     return 0;
