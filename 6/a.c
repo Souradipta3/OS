4e5dc82c9ebe40c529f13aab28913c6c1547612f
Souradipta Saha <137396269+Souradipta3@users.noreply.github.com>
2025-01-19 19:32:08 +0530

    Update a.c

--- 6/a.c
+++ 6/a.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
+int main() {
+    int pipe_fd[2];
+    pid_t pid;
+    char write_msg[] = "Hello from parent!";
+    char read_msg[50];
+
+    if (pipe(pipe_fd) == -1) {
+        printf("Pipe creation failed.\n");
+        return 1;
+    }
+
+    pid = fork();
+
+    if (pid < 0) {
+        printf("Fork failed.\n");
+        return 1;
+    }
+
+    if (pid == 0) {
+        // Child process
+        close(pipe_fd[1]); // Close unused write end
+        read(pipe_fd[0], read_msg, sizeof(read_msg)); // Read from pipe
+        printf("Child received: %s\n", read_msg);
+        close(pipe_fd[0]);
+    } else {
+        // Parent process
+        close(pipe_fd[0]); // Close unused read end
+        write(pipe_fd[1], write_msg, strlen(write_msg) + 1); // Write to pipe
+        close(pipe_fd[1]);
+    }
+
+    return 0;
+}
