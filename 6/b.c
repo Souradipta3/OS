a00aefe349ae20fbd5996f6dcb5a8ce6a8df1977
Souradipta Saha <137396269+Souradipta3@users.noreply.github.com>
2025-01-19 20:16:19 +0530

    Update b.c

--- 6/b.c
+++ 6/b.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <errno.h>
+#include <string.h>
 
+int main(int argc, char* argv[])
+{
+	int fd[2];
+	pid_t pid;
+	if (pipe(fd)==-1)
+	{
+		perror("pipe failed.\n");
+		exit(EXIT_FAILURE);
+	}
+	pid = fork();
+	if (pid<0)
+	{
+		perror("fork failed.\n");
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0)
+	{
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		execl("/bin/sort","sort",NULL);
+		perror("execl subroutine failed.\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		close(fd[0]);
+		dup2(fd[1],STDOUT_FILENO);
+		close(fd[1]);
+		execl("/bin/ls","ls",NULL);
+		perror("execl subroutine failed.\n");
+		exit(EXIT_FAILURE);
+	}
+	return 0;
+}
